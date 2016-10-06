8b168f5da6d2a8605d1017df93ad06f4d8637df6
ehopperdietzel <ehopperdietzel@gmail.com>
2016-10-06 11:14:09 -0300

    Nuevo

--- Codigo/public/js/menu.js
+++ Codigo/public/js/menu.js
@@ -1,35 +1,19 @@
+var chat = {
+  active:false,
+  open:function(){
+    $(".chat").slideDown(256,function(){
+      $(".messages").css('height','calc(100% - 420px)');
+    });
+    this.active = true;
+  },
+  close:function(){
+    $(".chat").slideUp(256);
+    $(".messages").css("height","100%");
+    this.active = false;
+  }
+};
+
+
 $(document).ready(function(){
-  var i = 0;
-  var dragging = false;
-     $('#dragbar').mousedown(function(e){
-         e.preventDefault();
 
-         dragging = true;
-         var main = $('#main');
-         var dragbar = $("#dragbar");
-         var ghostbar = $('<div>',
-                          {id:'ghostbar',
-                           css: {
-                                  height: dragbar.outerHeight(),
-                                  width: dragbar.outerWidth(),
-                                  top: main.offset().top,
-                                  bottom: main.offset().bottom
-                                 }
-                          }).appendTo('body');
-
-          $(document).mousemove(function(e){
-            ghostbar.css("top",e.pageY+2);
-         });
-      });
-
-     $(document).mouseup(function(e){
-         if (dragging)
-         {
-             $('#sidebar').css("height",e.pageY+2);
-             $('#main').css("top",e.pageY+2);
-             $('#ghostbar').remove();
-             $(document).unbind('mousemove');
-             dragging = false;
-         }
-      });
 });
