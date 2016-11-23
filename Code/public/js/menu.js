2c6bb1095c9b4be79b1ba972d4b9eb7f08cf4f5e
jose8club <jose8club@gmail.com>
2016-11-23 15:14:09 -0300

    cambios loaclhost:2000

--- Code/public/js/menu.js
+++ Code/public/js/menu.js
@@ -1,3 +1,5 @@
+var currentSection = "users";
+var currentProfileSection = "publications";
 $(document).ready(function(){
   //-------------------------------------------------------------------------
   //-------------------------------------------------------------------------
@@ -5,12 +7,12 @@ $(document).ready(function(){
   //-------------------------------------------------------------------------
   //-------------------------------------------------------------------------
 
-  showSection("users"); // < - ESTE DEBEN CAMBIAR
+  showSection(currentSection); // < - ESTE DEBEN CAMBIAR
 
 
   $( window ).resize(function(size) {
     var width = $(document).width();
-    if(width < 965){
+    if(width < 995){
       $(".rightBar").hide();
       $(".middleBar").css({'min-width':'450px',"width":"calc(100vw - 245px)"});
     }
@@ -22,14 +24,41 @@ $(document).ready(function(){
   $(".topBarItem").click(function(){
     showSection($(this).attr("section"));
   })
+  $(".blackBackground .close").click(function(){
+    $(".blackBackground,.modal").fadeOut(200);
+  })
 });
 
 function showSection(id){
+  currentSection = id;
   $(".middleBar").hide().load(id + ".html",function(){
     $(this).fadeIn(200);
+
+    //Profile
+    $(".newPost").click(function(){
+      $(".blackBackground,.modal").fadeIn(200);
+    })
+    if(currentSection == "profile"){
+      function displaySection(section){
+        $(".sections .section").css({background:"none"});
+        $(".sections [section='"+section+"']").css({background:"#EEE"});
+        if(section != currentProfileSection){
+          $("."+currentProfileSection).hide();
+          $("."+section).show();
+        }
+        currentProfileSection = section;
+      }
+      $(".sections .section").click(function(){
+        displaySection($(this).attr("section"));
+      });
+      displaySection(currentProfileSection);
+    }
+
+
   });
   $(".topBarItem .selectionLine").hide(200);
   $(".topBarItem[section='"+id+"']").children(".selectionLine").show(200);
+
 }
 var chat = {
   active:false,
